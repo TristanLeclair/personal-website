7e9477a246e03c714e9bc3cef8422c2235d45680
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2024-06-25 07:25:04 -0400

    add full project list in accordions

--- js/index.js
+++ js/index.js
@@ -78,3 +78,20 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const accordionButtons = document.querySelectorAll(".accordion-button");
+
+  accordionButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const content = this.nextElementSibling;
+
+      // Toggle display
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  });
+});
